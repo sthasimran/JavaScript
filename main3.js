8db9afe697da0ea93fefbd5bfe3f45f21be7a586
Simran Shrestha <simranstha2u@gmail.com>
2024-01-26 21:16:25 +0545

    declare object and array

---
+++ main3.js
@@ -0,0 +1,26 @@
+// To create object:
+
+const student1={
+    name:"Simran Shrestha",
+    address:"Raniban",
+    phoneNo:"9840760301",
+};
+console.log(student1);
+
+// keyValue:data
+
+const student2={
+    name:"Prachi",
+    address:"Dillibazr",
+    phoneNo:"9840760511",
+};
+console.log(student2);
+
+// Array Declaration:
+const arr1=[1,2,3,4];
+const arr2=["1","2","3","4"];
+const arr3=[student1,student2];
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
