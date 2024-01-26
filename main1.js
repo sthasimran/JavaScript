c152a9d0fd07897c5fb532ce2f7c992c045e7200
Simran Shrestha <simranstha2u@gmail.com>
2024-01-26 21:13:11 +0545

    script file added

---
+++ main1.js
@@ -0,0 +1,18 @@
+console.log("Main1 js is visible");
+
+console.log("Third line code");
+
+// const=constant we cannot reassign and redeclare
+// let= variable can be reassign but not redeclare
+// var= can reassign as well as redeclare
+
+const a=5;
+
+let b=6;
+b=7;
+
+var c=8;
+
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ main2.js
@@ -0,0 +1,40 @@
+// int + int=int
+// string + string = conactenate
+// string + int=string  'concatenation'
+console.log("main2 js visible");
+
+const first=1;
+const second=2;
+const sum= first+second;
+console.log(sum);
+
+const first1="Apple";
+const second1="Ball";
+const spacing=" "
+const sum1= first1+spacing+second1;
+console.log(sum1);
+
+const first2=200;
+const second2="Apple";
+const sum2= first2+second2;
+console.log(sum2);
+
+const first3=2;
+const second3="100";
+const sum3= first3+second3;
+console.log(sum3);
+
+// Type Conversion
+// wrap with "Number" or "String"
+
+const sumType= first2+Number(second2);
+console.log(sumType);
+
+const second4=Number("400");
+console.log(second4);
+
+const sumTpye1=String(first3)+Number(second3);
+console.log(sumTpye1);
+
+// Way to convert number to string:
+const first4=200+" ";
